626f5316ef81ba2bfc38e9d3a309bead4855703b
Matheus <matheuspsilva29@gmail.com>
2022-05-05 18:26:58 -0300

    Configuração de banco de dados e criação de entidade Pessoa

--- src/main/java/com/matheussilvadev/springbootmvc/SpringbootMvcApplication.java
+++ src/main/java/com/matheussilvadev/springbootmvc/SpringbootMvcApplication.java
@@ -2,8 +2,10 @@ package com.matheussilvadev.springbootmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.matheussilvadev.springbootmvc.model")
 public class SpringbootMvcApplication {
 
 	public static void main(String[] args) {
