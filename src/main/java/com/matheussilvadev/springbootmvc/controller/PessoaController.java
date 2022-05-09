aa773b0ac328bfe893a2a4e692a4b1646a14782f
Matheus <matheuspsilva29@gmail.com>
2022-05-09 11:40:49 -0300

    Listar Pessoas

--- src/main/java/com/matheussilvadev/springbootmvc/controller/PessoaController.java
+++ src/main/java/com/matheussilvadev/springbootmvc/controller/PessoaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.matheussilvadev.springbootmvc.model.Pessoa;
 import com.matheussilvadev.springbootmvc.repository.PessoaRepository;
@@ -23,6 +24,15 @@ public class PessoaController {
 	public String salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
 		return "cadastro/cadastropessoa";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/pessoas")
+	public ModelAndView pessoas() {
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		modelAndView.addObject("pessoas", pessoasIt);
+		
+		return modelAndView;
 	}
 	
 }
