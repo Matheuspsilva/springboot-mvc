e768cd6e0a9cfcb3c0dd776762462f36997412b0
Matheus <matheuspsilva29@gmail.com>
2022-05-25 17:32:44 -0300

    Configurando Spring Security em memória

---
+++ src/main/java/com/matheussilvadev/springbootmvc/security/WebConfigSecurity.java
@@ -0,0 +1,42 @@
+package com.matheussilvadev.springbootmvc.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends  WebSecurityConfigurerAdapter{
+	
+	@Override //Configura as solicitações de acesso por http
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable() //Desativa configurações padrões de memória.
+		.authorizeRequests() //Permitir/Restringir acessos
+		.antMatchers(HttpMethod.GET, "/").permitAll() //Permitindo acesso à página inicial
+		.anyRequest().authenticated()
+		.and().formLogin().permitAll() // Permitindo qualquer usuário acessar formulário de login
+		.and().logout()//Mapeia url de logout e invalida usuário autenticado
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+	}
+	
+	@Override //Cria autenticação do usuário com banco de dados ou em memória
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.passwordEncoder(NoOpPasswordEncoder.getInstance())// Senha em texto puro
+		.withUser("Matheus")
+		.password("123")
+		.roles("ADMIN");
+	}
+	
+	@Override //Ignora URL's específicas
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/materialize/**");
+	}
+	
+}
