9268acd5092e39a9fb1191ca0fdc939fc873c675
Matheus <matheuspsilva29@gmail.com>
2022-05-30 17:23:41 -0300

    Consumindo Web Service RESTful CEP

--- src/main/java/com/matheussilvadev/springbootmvc/model/Pessoa.java
+++ src/main/java/com/matheussilvadev/springbootmvc/model/Pessoa.java
@@ -40,6 +40,19 @@ public class Pessoa implements Serializable {
 	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL)
 	private List<Telefone> telefones;
 	
+	private String cep;
+	
+	private String rua;
+	
+	private String bairro;
+	
+	private String cidade;
+	
+	private String uf;
+	
+	private String  ibge;
+	
+	
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
@@ -78,6 +91,54 @@ public class Pessoa implements Serializable {
 	
 	public List<Telefone> getTelefones() {
 		return telefones;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getIbge() {
+		return ibge;
+	}
+
+	public void setIbge(String ibge) {
+		this.ibge = ibge;
 	}
 
 }
